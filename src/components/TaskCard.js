4994c252f41be19731dcef5360af7e23fc599eb9
Mike <supahotboi69@gmail.com>
2023-03-18 23:11:15 -0400

    bookmark styling on task cards

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -6,17 +6,32 @@ const TaskCard = ({ title, text, color }) => {
         backgroundColor: color
     }
 
+    const rightTriangleStyle = {
+        background: `linear-gradient(to right bottom, ${color} 0 50%, transparent 50% 100%)`
+    }
+
+    const leftTriangleStyle = {
+        background: `linear-gradient(to left bottom, ${color} 0 50%, transparent 50% 100%)`
+    }
+
     const [face, setFace] = useState(false)
 
     return (
-        <div className='task-card' style={cardStyle}
+        <div className='task-card'
              onClick={() => {
                 window.location.href = '/task'
              }}
              onMouseOver={() => setFace(true)}
              onMouseOut={() => setFace(false)}>
+            <div className='task-card-text' style={cardStyle}>
             {face ? <p>{text}</p> :
                     <h1>{title}</h1>}
+            </div>
+            {face ? 
+            <div className='task-card-triangles'>
+                <div className='task-card-right-triangle' style={rightTriangleStyle}></div>
+                <div className='task-card-left-triangle' style={leftTriangleStyle}></div>
+            </div> : <div></div>}
         </div>
     )
 }
